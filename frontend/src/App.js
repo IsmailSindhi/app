0af2f7e508b37dd120f1ef750241dfa55425c8ab
unknown <muhammadismailsoomro09@gmail.com>
2022-06-16 04:56:35 -0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import './App.css';
+import Home from './component/Home';
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+          <Route index path="/" element={ <Home/> }/>
+    </Routes>
+  </>
+  );
+}
+
+export default App;
+
+
---
+++ frontend/src/store.js
@@ -0,0 +1,25 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({
+  user: userReducer,
+  profile: profileReducer,
+  forgotPassword: forgotPasswordReducer,
+  allUsers: allUsersReducer,
+  userDetails: userDetailsReducer,
+});
+
+// let initialState = {
+//   },
+// };
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
